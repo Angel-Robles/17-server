4996ff8e06d355d7d8c8144c8cdfa401b892344c
AngelRobles06 <>
2023-03-29 13:56:50 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+        echo '<p> Angel Alejandro Robles Peralta </p>'; 
+        echo '<p> Calle Venustiano Carranza </p>';
+        echo '<p> #145585 </p>';
+        echo '<p> Colonia Quaso </p>';
+        echo '<p> Municipio Irak </p>';
+        echo '<p> Telefono +964 455 526 4569'
+    ?> 
+  </body>
+</html>
